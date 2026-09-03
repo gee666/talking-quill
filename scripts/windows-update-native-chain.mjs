e41a4e9eab12849e602a1df3c6bc62255e3606ce
geetesha <oira666@gmail.com>
2026-09-03 13:30:55 -0700

    Fix protected signer snapshot publication

--- scripts/windows-update-native-chain.mjs
+++ scripts/windows-update-native-chain.mjs
@@ -248,7 +248,7 @@ function protectSnapshot(path) {
 $ErrorActionPreference='Stop'
 $path=$TargetPath
 $sid=[Security.Principal.WindowsIdentity]::GetCurrent().User.Value
-& "$env:SystemRoot\System32\icacls.exe" $path '/inheritance:r' '/grant:r' '*S-1-5-18:(OI)(CI)F' '*S-1-5-32-544:(OI)(CI)F' "*$sid:(OI)(CI)RX" '/T' '/C' | Out-Null
+& "$env:SystemRoot\System32\icacls.exe" $path '/inheritance:r' '/grant:r' '*S-1-5-18:(OI)(CI)F' '*S-1-5-32-544:(OI)(CI)F' "*$($sid):(OI)(CI)RX" '/T' '/C' | Out-Null
 if($LASTEXITCODE-ne 0){throw 'ACL publication failed'}
 $items=@(Get-Item -LiteralPath $path)+(Get-ChildItem -LiteralPath $path -Force)
 foreach($item in $items){$acl=Get-Acl -LiteralPath $item.FullName;if(-not $acl.AreAccessRulesProtected){throw 'ACL inheritance'};if(($item.Attributes-band [IO.FileAttributes]::ReparsePoint)-ne 0){throw 'reparse'}}
